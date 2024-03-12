4f98294b5a062a31456a65c1e1b209e29276417f
Michael Liao <michael.liao@centre.edu>
2024-03-11 23:21:47 -0400

    created test file

---
+++ src/test/java/DataTests.java
@@ -0,0 +1,21 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class DataTests
+{
+	// can skip testing getters/setters/object equality (most) because auto-generated!
+
+	@BeforeEach
+	void setUp() throws Exception
+	{
+	}
+
+	@Test
+	void test()
+	{
+		fail("Not yet implemented");
+	}
+
+}
